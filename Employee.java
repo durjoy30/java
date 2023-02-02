1b9d2e82f8dfbfa54cab6ca642613ecfded0c403
Durjoy lal sarker <124159706+durjoy30@users.noreply.github.com>
2023-02-02 02:44:29 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,10 @@
+
+
+public class Employee{
+    int salary;
+    String name;
+    void show(){
+          system.out.println(name);
+          system.out.println(salary);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e1=new Employee ();
+        e1.name ="Durjoy";
+        e1.salary =4000;
+        e1.show ();
+
+    }
+
+}
